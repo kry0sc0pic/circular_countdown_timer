ec529a8b4d7ad2fb863e2df992692b4d3976ffbf
Krishaay Jois <krishaayjois@gmail.com>
2022-12-21 22:12:58 +0530

    Update countdown_text_format.dart

--- lib/countdown_text_format.dart
+++ lib/countdown_text_format.dart
@@ -2,6 +2,7 @@
 class CountdownTextFormat {
   static const String HH_MM_SS = "HH:mm:ss";
   static const String MM_SS = "mm:ss";
+  static const String MM_SS_ONLY = "mm:ss_alt"
   static const String SS = "ss";
   static const String S = "s";
 }
